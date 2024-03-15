b526bc588d28ff3505acd1f45e0f1eb7e2e854b6
swoichha adhikari <swoichha@swoichhas-MacBook-Pro.local>
2024-03-15 20:28:33 +0545

    added search bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import NavBar from './components/NavBar';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <>
       <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
-        <NavBar/>
+        <NavBar />
+        <SearchBar/>
       </div>
 
     </>
