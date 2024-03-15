4e125d61371fe0c14eae9d7bb4c049fa979bc976
swoichha adhikari <swoichha@swoichhas-MacBook-Pro.local>
2024-03-15 18:39:27 +0545

    added navbar with fixe cities

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
+      <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+        <NavBar/>
+      </div>
+
     </>
   );
 }
