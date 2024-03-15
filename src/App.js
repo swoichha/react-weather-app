e642ae3b021fa878132a3573baf8027bace4ca70
swoichha adhikari <swoichha@swoichhas-MacBook-Pro.local>
2024-03-15 20:38:10 +0545

    added time and location component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import NavBar from './components/NavBar';
 import SearchBar from './components/SearchBar';
+import { TimeAndLocation } from './components/TimeAndLocation';
 
 function App() {
   return (
     <>
       <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
         <NavBar />
-        <SearchBar/>
+        <SearchBar />
+        
+        <TimeAndLocation/>
       </div>
 
     </>
---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const TimeAndLocation = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">
+          Friday, 15 March 2024 | Local time: 8:45 PM
+        </p>
+      </div>
+
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">Kathmanu, Nepal</p>
+      </div>
+    </div>
+  );
+};
