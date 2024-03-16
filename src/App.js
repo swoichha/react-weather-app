71c06f04579f6413dc2e8c4fc0ff11d26f221a70
swoichha adhikari <swoichha@swoichhas-MacBook-Pro.local>
2024-03-16 17:05:27 +0545

    change background based on temp

--- src/App.js
+++ src/App.js
@@ -23,9 +23,18 @@ function App() {
     fetchWeather();
   }, [query, units])
 
+
+  const formatBackground = () => {
+    if (!weather) return "from-cyan-700 to-blue-700";
+    const threshold = units === "metric" ? 20 : 60;
+    if (weather.temp <= threshold) return "from-cyan-700 to-blue-700";
+
+    return "from-yellow-700 to-orange-700";
+  };
+  
   return (
     <>
-      <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <div className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}>
         <NavBar setQuery={setQuery} />
         <SearchBar setQuery={setQuery} units={units} setUnits={setUnits} />
         
